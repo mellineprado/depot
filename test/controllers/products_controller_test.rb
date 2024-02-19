5d33b57fa47b34ae1381c36cd13b8381355ad743
melline prado <mellinevprado@hotmail.com>
2024-02-19 16:24:17 -0300

    added logic that causes a product to be posted to a cart, causing a new line item to be created

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -46,10 +46,18 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
         description: @product.description,
         image_url: @product.image_url,
         price: @product.price,
-        title: @title,
+        title: @title
       }
     }
     assert_redirected_to product_url(@product)
+  end
+
+  test "can't delete product in cart" do
+    assert_difference("Product.count", 0) do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
   end
 
   test "should destroy product" do
