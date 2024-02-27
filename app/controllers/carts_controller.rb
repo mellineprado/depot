47dc38375bd672a926a3e57c51b10edd8818023f
melline prado <mellinevprado@hotmail.com>
2024-02-26 21:21:55 -0300

    added flash message when emptying a cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,31 +1,25 @@
 class CartsController < ApplicationController
-  before_action :set_cart, only: %i[ show edit update destroy ]
+  before_action :set_cart, only: %i[show edit update destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_cart
 
-  # GET /carts or /carts.json
   def index
     @carts = Cart.all
   end
 
-  # GET /carts/1 or /carts/1.json
-  def show
-  end
+  def show; end
 
-  # GET /carts/new
   def new
     @cart = Cart.new
   end
 
-  # GET /carts/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /carts or /carts.json
   def create
     @cart = Cart.new(cart_params)
 
     respond_to do |format|
       if @cart.save
-        format.html { redirect_to cart_url(@cart), notice: "Cart was successfully created." }
+        format.html { redirect_to cart_url(@cart), notice: 'Cart was successfully created.' }
         format.json { render :show, status: :created, location: @cart }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -34,11 +28,10 @@ class CartsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /carts/1 or /carts/1.json
   def update
     respond_to do |format|
       if @cart.update(cart_params)
-        format.html { redirect_to cart_url(@cart), notice: "Cart was successfully updated." }
+        format.html { redirect_to cart_url(@cart), notice: 'Cart was successfully updated.' }
         format.json { render :show, status: :ok, location: @cart }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -47,24 +40,28 @@ class CartsController < ApplicationController
     end
   end
 
-  # DELETE /carts/1 or /carts/1.json
   def destroy
-    @cart.destroy
+    @cart.destroy if @cart.id == session[:cart_id]
+    session[:cart_id] = nil
 
     respond_to do |format|
-      format.html { redirect_to carts_url, notice: "Cart was successfully destroyed." }
+      format.html { redirect_to store_index_url, notice: 'Your cart is currently empty' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_cart
-      @cart = Cart.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def cart_params
-      params.fetch(:cart, {})
-    end
+  def set_cart
+    @cart = Cart.find(params[:id])
+  end
+
+  def cart_params
+    params.fetch(:cart, {})
+  end
+
+  def invalid_cart
+    logger.error "Attempt to access invalid cart #{params[:id]}"
+    redirect_to store_index_url, notice: 'Invalid cart'
+  end
 end
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,9 @@
 class LineItem < ApplicationRecord
+  # associations
   belongs_to :product
   belongs_to :cart
+
+  def total_price
+    product.price * quantity
+  end
 end
